16f03459730d54ddd4484c9259c32c59209bba24
brendsoft <58312886+brendsoft@users.noreply.github.com>
2020-02-10 15:39:56 -0500

    whatsapp flotante
    activae boton whastapp

--- resources/views/tenant/layouts/partials/sidebar.blade.php
+++ resources/views/tenant/layouts/partials/sidebar.blade.php
@@ -392,5 +392,21 @@ $path)> 0)?$path[2]:''; $path[0] = ($path[0] === '')?'documents':$path[0];
                 }
             }
         </script>
+
+        <!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "51933038225", // WhatsApp number
+            call_to_action: "Soporte LIZA POS !", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
     </div>
 </aside>
--- resources/views/tenant/pos/register_layout.blade.php
+++ resources/views/tenant/pos/register_layout.blade.php
@@ -147,4 +147,19 @@
 <!-- Theme Base, Components and Settings -->
 <script src="{{asset('porto-light/js/theme.js')}}"></script>
 </body>
+        <!-- GetButton.io widget -->
+        <script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "51933038225", // WhatsApp number
+            call_to_action: "Soporte LIZA POS !", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
 </html>
